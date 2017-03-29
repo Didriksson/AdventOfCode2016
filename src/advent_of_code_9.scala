9b8380a7173420360ca5857dbdd87c8ce1725f73
Didriksson <mattias.didriksson@gmail.com>
2017-03-29 17:01:17 +0200

    Formattering på AoC9

--- src/advent_of_code_9.scala
+++ src/advent_of_code_9.scala
@@ -1,35 +1,36 @@
 import scala.io.Source
 
-
 object ComputeString extends App {
   val stringToDecode = {
-  val filepath = "absolute/Path/To/Data.txt"
-  Source.fromFile(filepath).getLines.mkString
-}
+    val filepath = "absolute/Path/To/Data.txt"
+    Source.fromFile(filepath).getLines.mkString
+  }
   println(decodeString(stringToDecode).length())
-      
+
   def decodeString(string: String): String = {
-     val opMarkerIndex = getMarkerLocation(string)
-     if(opMarkerIndex.isEmpty){
-       string
-     }
-     else{
-        val indexOfMarker = opMarkerIndex.get
-        val marker = string.substring(indexOfMarker._1, indexOfMarker._2).split("x").map(_.toInt)
-        string.take(indexOfMarker._1-1) + string.drop(indexOfMarker._2+1).take(marker(0))*marker(1) + decodeString(string.substring((indexOfMarker._2 + 1  + marker(0))))
-     }
-   }
-  
-    def getMarkerLocation(string: String) = {
-     val startOfMarker = string.indexOf('(') + 1
-     val endOfMarker = string.indexOf(')')
-     if(startOfMarker == -1 || endOfMarker == -1)
-       None
-     else 
-       Some((startOfMarker, endOfMarker))
-   }
+    val opMarkerIndex = getMarkerLocation(string)
+    if (opMarkerIndex.isEmpty) {
+      string
+    } else {
+      val indexOfMarker = opMarkerIndex.get
+      val marker = string
+        .substring(indexOfMarker._1, indexOfMarker._2)
+        .split("x")
+        .map(_.toInt)
+      string.take(indexOfMarker._1 - 1) + string
+        .drop(indexOfMarker._2 + 1)
+        .take(marker(0)) * marker(1) + decodeString(
+          string.substring((indexOfMarker._2 + 1 + marker(0))))
+    }
+  }
+
+  def getMarkerLocation(string: String) = {
+    val startOfMarker = string.indexOf('(') + 1
+    val endOfMarker = string.indexOf(')')
+    if (startOfMarker == -1 || endOfMarker == -1)
+      None
+    else
+      Some((startOfMarker, endOfMarker))
+  }
 
 }
-
-
-
