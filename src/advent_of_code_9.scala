65dcbfc17b54a061369ef69e8e3b8a65429415b9
Didriksson <mattias.didriksson@gmail.com>
2017-03-31 20:00:28 +0200

    Part 2

--- src/advent_of_code_9.scala
+++ src/advent_of_code_9.scala
@@ -2,25 +2,22 @@ import scala.io.Source
 
 object ComputeString extends App {
   val stringToDecode = {
-    val filepath = "absolute/Path/To/Data.txt"
+    val filepath = "/home/didde/workspace/AoC9/src/data.txt"
     Source.fromFile(filepath).getLines.mkString
-  }
-  println(decodeString(stringToDecode).length())
+  }.replaceAll("\\s", "")
+  println(decodeString(stringToDecode, 0))
 
-  def decodeString(string: String): String = {
+  def decodeString(string: String, length: Int): Int = {
+    println(string.length())
     val opMarkerIndex = getMarkerLocation(string)
     if (opMarkerIndex.isEmpty) {
-      string
+      length + string.length()
     } else {
       val indexOfMarker = opMarkerIndex.get
-      val marker = string
-        .substring(indexOfMarker._1, indexOfMarker._2)
-        .split("x")
-        .map(_.toInt)
-      string.take(indexOfMarker._1 - 1) + string
-        .drop(indexOfMarker._2 + 1)
-        .take(marker(0)) * marker(1) + decodeString(
-          string.substring((indexOfMarker._2 + 1 + marker(0))))
+      val marker = string.substring(indexOfMarker._1, indexOfMarker._2).split("x").map(_.toInt)
+      val addedWithMarker = string.take(indexOfMarker._1 - 1) + string.drop(indexOfMarker._2 + 1).take(marker(0)) * marker(1)
+      val newString = addedWithMarker + string.substring((indexOfMarker._2 + 1) + marker(0))
+      decodeString(newString.drop(indexOfMarker._1 - 1), length + indexOfMarker._1-1)
     }
   }
 
@@ -34,3 +31,6 @@ object ComputeString extends App {
   }
 
 }
+
+
+
