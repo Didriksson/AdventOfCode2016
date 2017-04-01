ab0caff7083b32d508b04a6a4d7cdbfa77c32556
Didriksson <mattias.didriksson@gmail.com>
2017-04-01 11:44:09 +0200

    Ingen bra lösning alls. Den tar dessutom lång tid. Cirkus 5min för att läsa ut svaret.

--- src/advent_of_code_9.scala
+++ src/advent_of_code_9.scala
@@ -5,29 +5,27 @@ object ComputeString extends App {
     val filepath = "/home/didde/workspace/AoC9/src/data.txt"
     Source.fromFile(filepath).getLines.mkString
   }.replaceAll("\\s", "")
-  println(decodeString(stringToDecode, 0))
-
-  def decodeString(string: String, length: Int): Int = {
-    println(string.length())
-    val opMarkerIndex = getMarkerLocation(string)
-    if (opMarkerIndex.isEmpty) {
-      length + string.length()
-    } else {
-      val indexOfMarker = opMarkerIndex.get
-      val marker = string.substring(indexOfMarker._1, indexOfMarker._2).split("x").map(_.toInt)
-      val addedWithMarker = string.take(indexOfMarker._1 - 1) + string.drop(indexOfMarker._2 + 1).take(marker(0)) * marker(1)
-      val newString = addedWithMarker + string.substring((indexOfMarker._2 + 1) + marker(0))
-      decodeString(newString.drop(indexOfMarker._1 - 1), length + indexOfMarker._1-1)
+ println(time { decodeString(stringToDecode) })
+  
+  def decodeString(string: String): Long = {
+    val marker_re = """[(](\d+)x(\d+)[)]""".r
+    marker_re.findFirstMatchIn(string) match {
+      case None => string.length()
+      case Some(m) => {
+        val addedWithMarker = m.start + decodeString(string.drop(m.end).take(m.group(1).toInt) * m.group(2).toInt)
+        val newString = string.substring((m.end) + m.group(1).toInt)
+        addedWithMarker + decodeString(newString)
+      }
     }
+
   }
 
-  def getMarkerLocation(string: String) = {
-    val startOfMarker = string.indexOf('(') + 1
-    val endOfMarker = string.indexOf(')')
-    if (startOfMarker == -1 || endOfMarker == -1)
-      None
-    else
-      Some((startOfMarker, endOfMarker))
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
+    result
   }
 
 }
